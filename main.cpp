431998b7f0babb9cfbb71362cde6efdefecf4be6
Marekse <m.trzeciak.p20@gmail.com>
2023-07-27 16:53:31 +0200

    moved existing main loop logic to game object

--- main.cpp
+++ main.cpp
@@ -5,29 +5,21 @@
 #include <math.h>
 #include <iostream>
 
-#include "src/utils/utils_2d.h"
-#include "src/classes/mapLoader.h"
-#include "src/classes/ball.h"
-#include "src/classes/wall.h"
-
+#include "src/classes/game.h"
+ 
 
 int main(){
 
-    // Window szouldn't be resizeable as it interferes with checking positions
+    // Window shouldn't be resizeable as it interferes with checking positions
     sf::RenderWindow window(sf::VideoMode(800, 700), "Balls and Walls", sf::Style::Close);        
     window.setFramerateLimit(60);
 
     sf::Vector2i mouse;
 
-    bool dragging = false;
-    const float max_drag = 100; 
-
-    Ball::window = &window;
-    Ball::W = window.getSize().x; 
-    Ball::H = window.getSize().y; 
-    Ball::friction = 0.9;
-
+    Game game(&window);
+    Ball::initialize(&window);
     loadMap(1);
+
 
     while(window.isOpen()){
 
@@ -36,72 +28,64 @@ int main(){
             if(event.type == sf::Event::Closed){
                 window.close();
             }else if(event.type == sf::Event::MouseButtonPressed){
-                mouse = sf::Mouse::getPosition(window);
-                for(auto &i : Ball::balls){        // checking if hovered over ball
-                    if(i.checkHover(mouse.x, mouse.y)){
-                        dragging = true;
-                        break;
-                    }
-                }
+                game.mousePress();   
             }
             if(event.type == sf::Event::MouseButtonReleased){
-                if(dragging){
-                    // Ball::active_ball -> setSpeed( mouse.x - sf::Mouse::getPosition(window).x, mouse.y - sf::Mouse::getPosition(window).y);
-                    Ball::active_ball -> setSpeed( (mouse.x - sf::Mouse::getPosition(window).x) / 3, (mouse.y - sf::Mouse::getPosition(window).y) / 3);
-                    Ball::active_ball -> body.setFillColor(Ball::active_ball -> color);
-                    dragging = false;
-                }    
+                game.mouseRelease();
             }
         }
 
         window.clear(sf::Color(102, 102, 102));
 
-        for(auto &i : Wall::walls){
-            window.draw(i.body);
-        }
-
-        for(auto &i : Ball::balls){        // updating positions
-            i.update();
-            window.draw(i.body);
-        }
-
-
-
-        if(dragging && Ball::movable){      // Drawing guide for trail
-            float distance = sqrt(pow(mouse.x - sf::Mouse::getPosition(window).x, 2) + pow(mouse.y - sf::Mouse::getPosition(window).y, 2));
-            double angle = bnw::getEquationAngle((sf::Vector2f)mouse, (sf::Vector2f)sf::Mouse::getPosition(window));
-            float x = cos(angle + M_PI_2) * Ball::active_ball -> body.getRadius() / 2;
-            float y = sin(angle + M_PI_2) * Ball::active_ball -> body.getRadius() / 2;
- 
-            sf::Vertex trail[3];
+        switch(game.getState()){
+            case Game::State::menu:
                 
-            if(distance < 200){
-                sf::Color trail_color((distance <= 100) ? (distance / 100 * 255) : (255), (distance >= 100) ? ((100 - distance) / 100 * 255) : (255), 0);
-                
-                trail[0] = sf::Vertex(sf::Vector2f(Ball::active_ball -> getPos().x + x, Ball::active_ball -> getPos().y + y), trail_color);
-                trail[1] = sf::Vertex(sf::Vector2f(Ball::active_ball -> getPos().x - x, Ball::active_ball -> getPos().y - y), trail_color);
-                trail[2] = sf::Vertex(sf::Vector2f(2 * (sf::Vector2i)Ball::active_ball -> getPos() - sf::Mouse::getPosition(window)), trail_color);
-            
-                Ball::active_ball -> body.setFillColor(trail_color);    
-            }else{      // Pushing velocity cap
-                trail[0] = sf::Vertex(sf::Vector2f(Ball::active_ball -> getPos().x + x, Ball::active_ball -> getPos().y + y), sf::Color::Red);
-                trail[1] = sf::Vertex(sf::Vector2f(Ball::active_ball -> getPos().x - x, Ball::active_ball -> getPos().y - y), sf::Color::Red);
-                float tip_x = cos(angle) * 200;
-                float tip_y = sin(angle) * 200;
-                
-                std::cout << sf::Mouse::getPosition(window).x << " " << Ball::active_ball -> getPos().x <<" " << angle << " \r\n";
 
-                if(sf::Mouse::getPosition(window).x < Ball::active_ball -> getPos().x +2.5){
-                    trail[2] = sf::Vertex(sf::Vector2f( Ball::active_ball -> getPos().x + tip_x, Ball::active_ball -> getPos().y + tip_y), sf::Color::Red);
-                }else{
-                    trail[2] = sf::Vertex(sf::Vector2f( Ball::active_ball -> getPos().x - tip_x, Ball::active_ball -> getPos().y - tip_y), sf::Color::Red);  
+                break;
+
+            case Game::State::map_selection:
+                
+
+
+                break;
+
+            case Game::State::settings:
+                
+
+
+                break;
+
+            case Game::State::playing:
+                for(auto &i : Wall::walls){
+                    window.draw(i.body);
                 }
 
-                Ball::active_ball -> body.setFillColor(sf::Color::Red);
-            }
+                for(auto &i : Ball::balls){        // updating positions
+                    i.update();
+                    window.draw(i.body);
+                }   
 
-            window.draw(trail, 3, sf::Triangles);
-            window.draw(Ball::active_ball -> body);       // might be optimizeable
+                if(game.calculateTrail()){
+                    window.draw(game.trail, 3, sf::Triangles);       
+                    window.draw(Ball::active_ball -> body);
+                }
+
+
+                break;
+
+            case Game::State::paused:
+                for(auto &i : Wall::walls){
+                    window.draw(i.body);
+                }
+
+                for(auto &i : Ball::balls){        // drawing without updating
+                    window.draw(i.body);
+                }   
+                break;
+
+            default:
+                game.errorReport("DRAW LOOP ERROR");
+                break;
         }
 
         window.display();
