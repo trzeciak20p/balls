4b33310e4653adc95288468c7520a5877a1251a5
abfipes <54113216+abfipes12@users.noreply.github.com>
2024-09-25 13:21:19 +0200

    Update compilerWarnings.cmake

--- cmake/compilerWarnings.cmake
+++ cmake/compilerWarnings.cmake
@@ -1,5 +1,4 @@
-
-# jeszcze nalezy to zrobić (pro tips)
+# TODO: jeszcze nalezy to zrobić (pro tips)
 
 # target_compile_options(main PRIVATE "-Wall;-Wextra;-Wpedantic;-pedantic-errors;-fanalyzer;-Wshadow;-Wnull-dereference;-Wduplicated-cond;-Wredundant-decls;-Wduplicated-branches;-Wlogical-op;-Wcast-qual;-Wfloat-equal")
 # target_compile_options(main PRIVATE "-Weverything;-Wno-c++98-compat")
